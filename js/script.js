76b1f456456c96c57a2fd1a90cd4dd1640679233
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-02-22 17:02:36 +0100

    realized project js(train-ticket)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
 
-const firstName = prompt("What's your first name?");
-document.getElementById("first-name").innerHTML = firstName;
 
-const lastName = prompt("What's your last name?");
-document.getElementById("last-name").innerHTML = lastName;
+let kmRoute = parseInt(prompt("How many kilometers does the route have?"));
+console.log(kmRoute);
 
-const favColor = prompt("What's your favourite color?");
-document.getElementById("fav-color").innerHTML = favColor;
+if (!(isNaN(kmRoute))) {
+    console.log("ok, the number insert is correct");
+    
+} else {
+    alert("The number of kilometers are not valid, the program insert a random value for you");
+    kmRoute = (Math.random() * 1000)+1;
+    console.log(kmRoute);
+}
 
-let genPsw = firstName + lastName + favColor + 21;
-document.getElementById("genereted-psw").innerHTML = genPsw;
+let agePassenger = Math.floor(parseInt(prompt("how old are the passenger?")));
+console.log(agePassenger);
+
+if (!isNaN(agePassenger) && agePassenger < 110) {
+    console.log("ok, the number insert is correct");
+    
+} else {
+    alert("The age is not valid, the program insert a random value for you");
+    agePassenger = Math.floor((Math.random() * 110))+1;
+    console.log(agePassenger);
+}
+
+const kmPrice = 0.21;
+
+let price = kmRoute * kmPrice;
+
+let discount = 0;
+
+if (agePassenger < 18) {
+    console.log(price);
+    discount = (price / 100) * 20;
+    price -= discount;
+    console.log("the price with the doscount is: " + price);
+
+}else if (agePassenger > 65) {
+    console.log(price);
+    discount = (price / 100) * 40;
+    price -= discount;
+    console.log("the price with the discount is: " + price);
+}else{
+    console.log(price);
+    console.log("you are not entitled for the discount");
+}
+
+price.toFixed(2); 
+
+document.getElementById("km-route").innerHTML = `${kmRoute.toFixed(2)} km`;
+
+document.getElementById("age-passenger").innerHTML = `${agePassenger}`;
+
+document.getElementById("discount").innerHTML = `${discount.toFixed(2)} €`;
+
+document.getElementById("ticket-price").innerHTML = `${price.toFixed(2)} €`;
